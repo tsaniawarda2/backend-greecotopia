deb926d84d6daefdda44c5cade77caf3db6d6bc9
deviayulestari <devialestari04@gmail.com>
2022-01-06 21:07:00 +0700

    fixing migrations participant

--- migrations/20211222080915-create-participant.js
+++ migrations/20211222080915-create-participant.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Participants', {
-=======
-"use strict";
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Participants", {
->>>>>>> 139d728766ad697b1ec41205a41fb7f803648e6c
       participant_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-<<<<<<< HEAD
         type: Sequelize.INTEGER
       },
       name: {
@@ -50,41 +42,3 @@ module.exports = {
     await queryInterface.dropTable('Participants');
   }
 };
-=======
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      no_hp: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      number_of_trees: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      user_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      tanam_pohon_id: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Participants");
-  },
-};
->>>>>>> 139d728766ad697b1ec41205a41fb7f803648e6c
